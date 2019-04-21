04a8bf161d5d2bd62ac907a5d10ddbd088316556
guruchian <30399464+semicon@users.noreply.github.com>
2019-04-21 19:31:54 +0700

    snakevovab
    snakevovab

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1555849303,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/alphabet-sheet0.png",
+		"images/alphabet-sheet1.png",
+		"images/alphabet-sheet2.png",
+		"images/box-sheet0.png",
+		"images/textshow.png",
+		"images/head-sheet0.png",
+		"images/body-sheet0.png",
+		"images/animal-sheet0.png",
+		"images/left_btn-sheet0.png",
+		"images/bggrass.png",
+		"images/right_btn-sheet0.png",
+		"images/particles.png",
+		"images/tiledbg.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
